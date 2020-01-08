f1c50d0d27c6315d3c38f94dfea7617125f64fe7
isaac_you <isaac_you@mhand.net>
2020-01-08 13:59:10 +0900

    change

--- step1/change.py
+++ step1/change.py
@@ -1,21 +1,11 @@
 a = int(input())
 
-
-
 change = 1000 - a
-coinType = [100, 50, 10]
-coinCounter = list(range(len(coinType)))
-for i in range(len(coinType)):
-  coinCounter[i] = int(change / coinType[i])
-  change = change % coinType[i]
 
-print(coinCounter)
+coin100 = change / 100
+change = change % 100
+coin50 = change / 50
+change = change % 50
+coin10 = change / 10
 
-
-# coin100 = change / 100
-# change = change % 100
-# coin50 = change / 50
-# change = change % 50
-# coin10 = change / 10
-
-# print("%d %d %d" % (coin100, coin50, coin10))
+print("%d %d %d" % (coin100, coin50, coin10))
